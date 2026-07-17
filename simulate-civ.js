5f71a897c78b69986129c21e5e9b1f8d1fdc04fb
Rafael Nascimento <rafael5asn@gmail.com>
2026-07-17 10:31:27 -0400

    classes experiment

--- simulate-civ.js
+++ simulate-civ.js
@@ -1,4 +1,17 @@
 
+class Vil {
+    constructor(spawn, id) {
+        this.id = id;
+        this.spawn = spawn;
+        this.work = "idle";
+        this.bag = 0;
+        this.findWork = function() {
+        }
+        this.dropOff = function() {
+        }
+    }
+}
+
 let defaultTimeInterval = 1;
 let timestamp = 0;
 let vils = [];
@@ -9,15 +22,26 @@ let tc = {
     }
 } 
 
+let entities = [
+    new Vil(1, 1),
+    new Vil(15, 2)
+]
+
 function simulateCiv(){
     runTimeCycle(defaultTimeInterval);
     runTimeCycle(defaultTimeInterval);
     console.log(timestamp);
     tc.chooseProd();
+    entities.forEach((entity) => {
+        console.log(entity);
+        if (entity.constructor.name == "Vil") {console.log("é vil")}
+    })
 }
 
 function runTimeCycle(interval) {
     timestamp += interval;
 }
+
+simulateCiv()
 
 module.exports = {simulateCiv}
